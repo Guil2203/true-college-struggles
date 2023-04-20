9b93894093679c77da60138f93737ab8fdda38e7
Gui2203 <127751360+Guil2203@users.noreply.github.com>
2023-04-20 11:37:55 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <center>
-    <form id="send" action="media.php" method="POST">
-    nota1:<input type="number" name="nota1">
-    <br>
-    nota2:<input type="number" name="nota2">
-    <br>
-    nota3:<input type="number" name="nota3">
-    <br>
-    <input type="submit" name="send" value="send">
-</form>
-</center>
-</body>
-</html>
+<?php
+/*localhost/guirib/index.php*/
+$variavel = 10;
+$variavel2 = "hey :)";
+
+echo $variavel."<br>";
+echo $variavel2."<br>";
+
+if ($variavel < 20){
+    echo "less than 20";
+} else {
+    echo "bigger than 20";
+}
+
+echo "<br><br>";
+echo "using For:<br>";
+for($i=0; $i<=10; $i++) {
+    echo $i." ";
+}
+
+echo "<br><br>";
+echo "using While:<br>";
+
+$j = 0;
+while($j <= 10) {
+    echo $j." ";
+    $j++;
+}
+echo "<br><br>";
+echo "usando do While<br>";
+$k = 0;
+do {
+    echo "hey ";
+    $k++;
+} while($k <= 10);
+
+echo "<br><br>";
+echo "usando switch case<br>";
+
+$switch = 1;
+switch ($switch) {
+    case 1: echo "number 1"
+        break;
+    
+    default:
+        echo "not the informed value"
+        break;
+}
+
+?>
