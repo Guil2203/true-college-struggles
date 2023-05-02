f58a144423af1345aa237e11f116fbdce1581f27
Gui2203 <127751360+Guil2203@users.noreply.github.com>
2023-05-02 11:37:39 -0300

    Add files via upload

---
+++ revisaoA1q3.php
@@ -0,0 +1,19 @@
+<?php
+
+/* q.3: escreva um código que receba 3 valores inteiros e os exiba em ordem crescente
+obs: os valores devem ser diferentes
+ex:
+valor1=10
+valor2=3
+valor3=14
+
+saída
+3 10 14*/
+
+$valor1=10;
+$valor2=3;
+$valor3=14;
+
+if ();
+
+?>
